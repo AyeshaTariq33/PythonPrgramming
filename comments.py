62de1744fbd10412770fd88a2e6d6c4669be1608
Ayesha3317 <vertexa1.00@gmail.com>
2025-05-28 20:19:21 +0500

    comments in python

--- comments.py
+++ comments.py
@@ -1,1 +1,6 @@
 #comments in python looks like this.
+"""
+hello. This is multi line commenting..... btw python doesnt have in perticular multiline
+coments but this metthod is used like  Python will ignore string literals that are not 
+assigned to a variable...... so we can use three times inverted commas for multiline commenting.
+"""
