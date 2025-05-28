405f82f1caf5c6d13757a7c452966add568472a0
Ayesha3317 <vertexa1.00@gmail.com>
2025-05-28 21:52:49 +0500

    assigning multiple values to variables

--- variables.py
+++ variables.py
@@ -1,22 +1,45 @@
-x=5
-y="Variables"
-print (x)
-print(type(x))
-print (y)
+W=5
+E="Variables"
+print (W)
+print(type(W))
+print (E)
 
 
-x=("variable types.....")
-print (x)
+D=("variable types.....")
+print (D)
+#Insert the correct syntax to assign values to multiple variables in one line
+A, B, C = "Orange", "Banana", "Cherry"
+
 
 #getting the datatype of variable.
-print(type(x))
-print(type(y))
+print(type(W))
+print(type(E))
 
-x='Mujtba'
-print (x)
+j='Mujtba'
+print (j)
 
 #case sensitive hai python
 t=(2006)
 T="Taha"
 print(t)
 print(T)
+
+
+#naming variables
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+x, y, z = "Kainat", "Nimra", "Ayesha"
+friends = ["Uswa", "Ezza", "Faiza"]
+a, b, c= friends
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+print(c)
